d7f90ddd264136b9751d1c054c213311469e14dc
Max Vaessen <maxvaessen@gmail.com>
2023-02-28 13:44:20 +0100

    remove indexes

--- database/migrations/2023_02_24_130611_create_translate_table.php
+++ database/migrations/2023_02_24_130611_create_translate_table.php
@@ -14,9 +14,9 @@ class CreateTranslateTable extends Migration
     public function up()
     {
         Schema::create('translate', function (Blueprint $table) {
-            $table->bigInteger('id')->primary()->unique();
+            $table->bigInteger('id')->primary();
             $table->bigInteger('steam_id');
-            $table->string('guid', 40)->index();
+            $table->string('guid', 40);
         });
     }
 
